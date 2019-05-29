b28d687cf2a6dcf072fd09c776000d6ca5212286
Alberto <albertomaronezio@hotmai.com>
2019-05-29 11:30:02 -0300

    Exercicio_Parte2_01

---
+++ Exercicio_Parte2_01/Exercicio_Parte2_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int sucessor = 0;
+
+            Console.Write("Digite um Número para saber o seu Sucessor: ");
+            numero = int.Parse(Console.ReadLine());
+
+            sucessor = numero + 1;
+
+            Console.WriteLine("O Sucessor do Número {0} é {1}", numero, sucessor);
+        }
+    }
+}
